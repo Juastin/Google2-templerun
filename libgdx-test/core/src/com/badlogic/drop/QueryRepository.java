557a3bdc299e31a8ba3fbf3cb28363d07d5f00d3
Whoooeoeo <brmkru@gmail.com>
2021-04-21 11:55:35 +0200

    Queries

--- libgdx-test/core/src/com/badlogic/drop/QueryRepository.java
+++ libgdx-test/core/src/com/badlogic/drop/QueryRepository.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class QueryRepository {
     static Connection verbinding = Database.maakVerbinding();
@@ -14,6 +15,29 @@ public class QueryRepository {
             PreparedStatement myStmt = verbinding.prepareStatement("insert into namen (gebruikersnaam) Select ? Where not exists(select * from namen where gebruikersnaam=?)");
             myStmt.setString(1, username);
             myStmt.setString(2, username);
+            Database.query(myStmt);
+        } catch (Exception e) {
+            System.out.println("Fout met opslaan naam");
+        }
+    }
+
+    public static ArrayList<ArrayList<String>> selectidfromname(String username) {
+        try {
+            PreparedStatement myStmt = verbinding.prepareStatement("SELECT id FROM namen WHERE gebruikersnaam=?");
+            myStmt.setString(1,username);
+            ArrayList<ArrayList<String>> results = Database.query(myStmt);
+            return results;
+        } catch (Exception e) {
+            System.out.println("Fout met ophalen van leaderboard");
+            return null;
+        }
+    }
+
+    public static void inserthighscore(int punten,String playerid){
+        try {
+            PreparedStatement myStmt = verbinding.prepareStatement("INSERT INTO highscore VALUES (?,?)");
+            myStmt.setInt(1, punten);
+            myStmt.setInt(2, Integer.parseInt(playerid));
             Database.query(myStmt);
         } catch (Exception e) {
             System.out.println("Fout met opslaan naam");
